b8019e7c64265db518c51f2ffbe5d6cefbfc5ccf
Karen Borrero <keba2503@gmail.com>
2022-01-30 20:55:46 +0000

    server

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://amanonweb.com',
+ 
   app: {
     keys: env.array('APP_KEYS'),
   },

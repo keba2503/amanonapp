8a208e09b4d914b211f66606dde01c500ad6fc56
Karen Borrero <keba2503@gmail.com>
2022-01-30 22:12:11 +0000

    cambios para conect api/strapi

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,8 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
- 
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c0695f3d1ac97643174e51a32d2c4145'),
+    },
   },
 });
